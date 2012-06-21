78376f1a5b38f3d78bad93ce96544adc74f33e30
Syntruth <syntruth@gmail.com>
2012-06-20 22:35:59 -0400

    3.0 beta push; not quite finished yet!

--- dicelib.rb
+++ dicelib.rb
@@ -1,7 +1,43 @@
+# Copyright (c) 2012 Randy Carnahan <syn at dragonsbait dot com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining 
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included 
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN 
+# NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+# USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# dicelib.rb -- version: 3.0
+
 require 'rubygems'
 require 'parslet'
 
-module Dice
+module DiceLib
+
+  DefaultRoll = "1d6"
+
+  DefaultOptions = {
+    :drop    => 0,
+    :keep    => 0,
+    :explode => 0,
+    :reroll  => 0,
+  }
+
+  ExplodeLimit = 20
+
+  class DiceLibError < Exception; end
+
   class Parser < Parslet::Parser
 
     # Base rules.
@@ -9,11 +45,24 @@ module Dice
     rule(:number)  { match('[0-9]').repeat(1) }
     rule(:number?) { number.maybe }
 
+    # Label rule
+    rule(:lparen) { str('(') }
+    rule(:rparen) { str(')') }
+    rule(:label) do
+      lparen >> 
+      match('[^()]').repeat(1).as(:label) >> 
+      rparen >> 
+      space?
+    end
+
     # count and sides rules.
     rule(:count) { number?.as(:count) }
     rule(:sides) { match('[dD]') >> number.as(:sides) }
 
-    # Roll Options.
+    # xDx Parts.
+    rule(:xdx) { (count >> sides).as(:xdx) >> options? }
+
+    # xdx Options.
     rule(:explode) { str('e') >> number?.as(:explode) }
     rule(:drop)    { str('~') >> number.as(:drop) }
     rule(:keep)    { str('!') >> number.as(:keep) }
@@ -25,59 +74,405 @@ module Dice
 
     rule(:options?) { options.maybe.as(:options) }
 
-    # Static modifiers.
+    # Part Operators.
     rule(:add) { str('+') }
     rule(:sub) { str('-') }
     rule(:mul) { str('*') }
     rule(:div) { str('/') }
+    rule(:op)  { (add | sub | mul | div).as(:op) }
+    
+    # Part
+    rule(:part)  do
+      space?                    >> 
+      op                        >> 
+      space?                    >> 
+      (xdx | number).as(:value) >> 
+      space?
+    end
 
-    rule(:op)   { (add | sub | mul | div).as(:op) }
-    rule(:mod)  { space? >> op >> space? >> number.as(:mod) >> space? }
-    rule(:mods) { mod.repeat.as(:mods) }
+    rule(:parts) { xdx.as(:start) >> part.repeat }
 
-    rule(:dice) { (count >> sides).as(:xdx) >> options? >> mods }
+    rule(:dice) { label.maybe >> parts  }
 
     root(:dice)
   end
 
   class Transform < Parslet::Transform
 
-    rule(:count => simple(:x)) { Integer(x) }
-    rule(:sides => simple(:x)) { Integer(x) }
+    def Transform.hashify_options(options)
+      opts = {}
+      options.each {|opt, val| opts[opt] = val}
+      return opts
+    end
 
-    rule(:explode => simple(:x)) { [:explode, Integer(x)] }
+    # Option transforms. For those in some xdx subtrees,
+    # the Transform.hashify_options method is called.
     rule(:drop    => simple(:x)) { [:drop,    Integer(x)] }
     rule(:keep    => simple(:x)) { [:keep,    Integer(x)] }
     rule(:reroll  => simple(:x)) { [:reroll,  Integer(x)] }
-
-    rule(:mod => simple(:m), :op => simple(:o)) do
-      op = case o
-      when '+' then :add
-      when '-' then :sub
-      when '*' then :mul
-      when '/' then :div
-      else :unknown
-      end
-
-      [op, Integer(m)]
+    
+    # Explode is special, in that if it is nil, then it
+    # must remain that way.
+    rule(:explode => simple(:x)) do
+      x.nil? ? [:explode, nil] : [:explode, Integer(x)]
     end
 
-    rule(:count => simple(:c), :sides => simple(:s)) do
-      {
-        :count => Integer(c),
-        :sides => Integer(s)
+    # Parts {:ops => (:xdx | :number)}
+    # These are first-match, so the simple number will
+    # be matched before the xdx subtree.
+    rule(:op => simple(:o), :value => simple(:v)) do
+      [String(o), Integer(v)]      
+    end
+
+    rule(:op => simple(:o), :value => subtree(:part)) do
+      [String(o), 
+        {
+          :xdx => {
+            :count => Integer(part[:xdx][:count]),
+            :sides => Integer(part[:xdx][:sides])
+          },
+          :options => Transform.hashify_options(part[:options])
+        }
+      ] 
+    end
+
+    rule(:label => simple(:s)) { {:label => String(s)} }
+
+    rule(:label => simple(:s), :start => subtree(:part)) do
+      [
+        {:label => String(s)},
+        {:start => {
+          :xdx     => part[:xdx],
+          :options => Transform.hashify_options(part[:options])
+          }
+        }
+      ]
+    end
+
+    rule(:start => subtree(:part)) do
+      {:start => {
+        :xdx     => part[:xdx],
+        :options => Transform.hashify_options(part[:options])
+        }
       }
+    end
+
+    # We have to match these this way; this is a pain 
+    # in the ass of a match. >:E
+    rule(:count => simple(:c), :sides => simple(:s)) do
+      { :count => Integer(c), :sides => Integer(s) }
     end
 
   end
 
+  # The most simplest of a part. If a given part of
+  # a dice string is not a Label, Fixnum, or a xDx part
+  # it will be an instance of this class, which simply
+  # returns the value given to it.
+  class SimplePart
+    attr :value
+
+    def initialize(part)
+      @value = part
+    end
+
+    def result
+      return @value
+    end
+
+    def to_s
+      return @value
+    end
+  end
+
+  # The subclass for a label.
+  class LabelPart < SimplePart
+    def to_s
+      return "(%s)" % self.value
+    end
+  end
+
+  # This represents a static, non-random number part
+  # of the dice string.
+  class StaticPart < SimplePart
+    def initialize(num)
+      num    = num.to_i() if num.is_a?(String)
+      @value = num
+    end
+
+    def total
+      return self.value
+    end
+
+    def to_s
+      return self.value.to_s()
+    end
+  end
+
+  # This represents the xDx part of the dice string.
+  class RollPart < SimplePart
+
+    attr :count
+    attr :sides
+    attr :parts
+
+    def initialize(part)
+      @total  = nil
+      @tally  = []
+      @value  = part
+      @count  = part[:xdx][:count]
+      @sides  = part[:xdx][:sides]
+
+      # Our Default Options
+      @options = {
+        :explode => 0,
+        :drop    => 0,
+        :keep    => 0,
+        :reroll  => 0
+      }
+
+      if part.has_key?(:options)
+        @options.update(part[:options])
+
+        # Check for nil :explode and set it
+        # to @sides.
+        @options[:explode] = @sides if @options[:explode].nil?
+      end
+    end
+
+    # Checks to see if this instance has rolled yet
+    # or not.
+    def has_rolled?
+      return @total.nil? ? false : true
+    end
+
+    # Rolls a single die from the xDx string.
+    def roll_die()
+      num    = 0
+      reroll = (@options[:reroll] >= self.sides) ? 0 : @options[:reroll]
+
+      while num <= reroll
+        num = rand(self.sides) + 1
+      end
+
+      return num
+    end
+
+    def roll
+      results = []
+
+      self.count.times do
+        roll = self.roll_die()
+
+        results.push(roll)
+
+        if @options[:explode] > 0
+          explode_limit = 0
+
+          while roll >= @options[:explode]
+            roll = self.roll_die()
+            results.push(roll)
+            explode_limit += 1
+            break if explode_limit >= ExplodeLimit
+          end
+        end
+      end
+
+      @tally = results.dup()
+
+      results.sort!
+
+      if @options[:drop] > 0
+        results = results[0 ... @options[:drop]]
+      end
+
+      results.reverse!
+
+      if @options[:keep] > 0
+        results = results[0 ... @options[:keep]]
+      end
+
+      # I think reduce(:+) is ugly, but it's very fast.
+      @total = results.reduce(:+)
+
+      return self
+    end
+
+    # Returns the tally from the roll. This is the entire
+    # tally, even if a :keep or :drop options was given.
+    def tally(do_sort=true)
+      do_sort ?  @tally.dup.sort.reverse() : @tally
+    end
+
+    # Gets the total of the last roll; if there is no 
+    # last roll, it calls roll() first.
+    def total
+      self.roll() if @total.nil?
+      return @total
+    end
+
+    # This takes the @parts hash and recreates the xDx
+    # string. Optionally, passing true to the method will
+    # remove spaces form the finished string.
+    def to_s(no_spaces=false)
+      s = ""
+
+      sp = no_spaces ? "" : " "
+      
+      s += self.count.to_s unless self.count.zero?
+      s += "d"
+      s += self.sides.to_s
+
+      unless @options[:explode].zero?
+        s += "#{sp}e"
+        s += @options[:explode].to_s unless @options[:explode] == self.sides
+      end
+
+      s += "#{sp}~" + @options[:drop].to_s   unless @options[:drop].zero?
+      s += "#{sp}!" + @options[:keep].to_s   unless @options[:keep].zero?
+      s += "#{sp}r" + @options[:reroll].to_s unless @options[:reroll].zero?
+
+      return s
+    end
+
+    def <=>(other)
+      return self.total <=> other.total
+    end
+  end
+
+  class Roll
+    attr :dstr
+    attr :tree
+
+    def initialize(dstr=nil)
+      @dstr   = dstr ||= DefaultRoll
+      @tree   = DiceLib.parse(dstr)
+      @result = nil
+    end
+
+    def result
+      self.roll() unless @result
+      return @result
+    end
+
+    def roll
+      total    = 0
+      label    = ""
+      sections = []
+    
+      self.tree.each do |op, part|
+        do_push = true
+ 
+        # If this is a RollPart instance,
+        # ensure fresh results.
+        part.roll() if part.is_a?(RollPart)
+
+        case op
+        when :label
+          label   = part.value()
+          do_push = false
+        when :start
+          total = part.total()
+        when :add
+          total += part.total()
+        when :sub
+          total -= part.total()
+        when :mul
+          total *= part.total()
+        when :div
+          total /= part.total()
+        end
+
+        sections.push(part) if do_push
+      end
+
+      @result = Result.new(label, total, sections)
+
+      return @result
+    end
+  end
+
+  class Result 
+    attr_reader :label
+    attr_reader :total
+    attr_reader :sections
+
+    def initialize(label, total, sections)
+      @label    = label
+      @total    = total
+      @sections = sections
+    end
+  end
+
   def self.normalize_tree(tree)
-    tree[:count] = tree[:xdx][:count]
-    tree[:sides] = tree[:xdx][:sides]
+    return tree.collect do |part|
+      case part
+      when Hash
+        if part.has_key?(:label)
+          part = [:label, LabelPart.new(part[:label])]
+        elsif part.has_key?(:start)
+          xdx  = normalize_xdx(part[:start])
+          part = [:start, RollPart.new(xdx)]
+        end
 
-    tree.delete(:xdx)
+      when Array
+        op = case part.first
+        when "+" then :add
+        when "-" then :sub
+        when "*" then :mul
+        when "/" then :div
+        else part.first
+        end
 
-    return tree
+        val = part.last
+        val = val.is_a?(Hash) ? RollPart.new(val) : StaticPart.new(val)
+
+        part = [op, val]
+      end
+
+      part
+    end
+  end
+
+  def self.normalize_xdx(xdx)
+    if xdx[:options].to_s.strip.empty?
+      xdx.delete(:options)
+    else
+      case xdx[:options]
+      when Hash
+        opts = {}
+
+        xdx[:options].each do |opt, value|
+          opts[opt] = value
+        end
+
+        xdx[:options] = opts
+
+      when Array
+        # Seriously, somehow these are not being
+        # transformed from the parsing. No clue why.
+        # This might be fixed now!
+        opts = Transform.hashify_options(xdx[:options])
+        xdx[:options] = opts
+      end
+    end
+
+    return xdx
+  end
+
+  def self.parse(dstr="")
+    begin
+      tree = Parser.new.parse(dstr)
+      ast  = Transform.new.apply(tree)
+      ast  = normalize_tree(ast)
+
+    rescue Parslet::ParseFailed => reason
+      # We're merely re-wrapping the error here to 
+      # hide implementation from user who doesn't care
+      # to read the source.
+      #raise DiceLibError, "Dice Parse Error: #{reason}"
+      STDERR.write("Error: #{reason}\n")
+    end
   end
 end 
 
@@ -86,21 +481,22 @@ if $0 == __FILE__
 
   dstrs = [
     # Basic rolls.
-    '2d10', '1D20', 'd8',
-    '1d20 +5', '2d4+2-1',
+    '(Damage) 2d10', 
+    '4d6!3',
     
     # Complex ones!
     '5d6!3e + 4 - 1', 
-    '6d12~2e10 +5 + 3'
+    '(Complex!) 6d10~2e10 +5 + 1d6 r3 - 2'
   ]
 
   dstrs.each do |dstr|
     puts "Trying #{dstr}"
 
-    tree = Dice::Parser.new.parse(dstr)
-    ast  = Dice::Transform.new.apply(tree)
-    
-    pp Dice.normalize_tree(ast)
+    roll = DiceLib::Roll.new(dstr)
+    res  = roll.result()
+
+    pp roll.tree
+    puts "#{dstr}: #{res.total}"
     puts ""
   end
 end
--- readme.md
+++ readme.md
@@ -5,7 +5,7 @@ Ruby Dice Lib
 
 **Author :** Randy Carnahan
 
-**Version:** 3.0b
+**Version:** 3.0
 
 **License:** LGPL OR MIT
 
@@ -16,10 +16,25 @@ to every gamer's (RPG and otherwise) need to have dice rolled. The
 centralized concept to this is taking a standard formatted string and
 parsing it, returning values from that string.
 
+The original version of this library was the 'rolldice' Unix
+command-line application. Since this, it's been added to to allow
+additional elements in the dice string.
+
+Note: prior versions of this library allowed for dice strings to be
+"complex" in that there was more than one dice string, separated by
+commas, to be parsed and returned. This feature was never really used,
+so it's been removed to keep this newer version (3.0+) cleaner.
+
+Starting with Version 3.0, this library now uses parslet, the excellent
+Ruby syntax parser. It's made the internals a *bit* more complicated,
+but has allowed greater flexibility in constructing dice strings. For 
+example, previous version, you had to do '4d6 e6 !3' if you wanted the
+dice to explode (see below) and to keep the 3 highest rolls. Now, the
+e6 and !3 (and the other options) can be in any order after the xDx
+part of the string.
+
 Dice Strings
 ------------
-
-There are two types of dice strings in this library:
 
 **Simple Dice String**
 
@@ -27,18 +42,27 @@ A simple dice string, also called the xDx string, that represents a
 single group of dice, such as 3d10 or 4d6. Optional parts of the 
 Simple Dice string are given below.
 
-**Complex Dice Strings**
+A dice string is made up of one or more parts that consist of either:
 
-A complex dice string is a string that is made up of one or more
-Simple Dice strings, static +/- values, and optional Label parts, 
-separated by commas.
+- an optional label, which must be the first part of the string.
+  Labels are defined within parenthesis.
+- an xDx (such as 3d6) definition and options for that definition.
+- a static modifier that is applied to the current total.
 
-An example of a complex dice string is:
+The allowed static modifiers are add (+), subtract (-), multiply (\*),
+and divide (/). As the library (in the Roll class) enumerates over the
+parsed tree, a total for that roll is kept; the static modifiers are 
+applied *in the order they are gotten*, which is to say, the standard
+order of arithmetic calculations do not apply. 
 
-  (Attack) 1d20 + 8, (Damage) 2d4 + 7 + 1d6 - 2 
+For example:
 
-Note, that spaces in the strings are optional, but the string is 
-easier to read if there are spaces.
+    2d6 + 5 * 3 - 6
+
+...would roll the 2d6 for the total, then add 5 to it, then multiply
+that total by 3, and finally subtract 6. The 5 is not multiplied by 3
+for a 15 and then added to the roll result. Just something to keep in
+mind.
 
 Simple Dice String Options
 --------------------------
@@ -46,19 +70,21 @@ Simple Dice String Options
 In the following section, note that # is used to denote the number
 part of a option.
 
-The options are given in the order they should appear in the simple
-dice string.
-
 **xDx** - denotes how many dice to roll and how many sides the dice
 should have. This is the standard RPG dice syntax. Note that if the 
 sides of the die is given as '%' it will be converted to 100 
-automatically.
+automatically. This *must* come before any options for a given set of
+dice.
 
 **e#** - the explode value. Some game systems call this 'open ended'
 dice. If the number rolled is greater than or equal to the value given
 for this option, the die is rolled again and added to the total. If no
 number is given for this option, it is assumed to be the same as the
-number of sides on the die. Thus, '1d6e6' is the same as '1d6e'.
+number of sides on the die. Thus, '1d6e' is the same as '1d6e6'.
+
+**~#** - this denotes how many dice to drop from the tally. These dice
+are dropped *before* any dice are kept with !# below. So, '5d6 ~2' 
+means roll five 6-sided dice and drop the lowest 2 values.
 
 **!#** - this denotes how many dice to keep out of the number of dice
 rolled, keeping the highest values from the roll. Thus, '4d6 !3' means
@@ -69,12 +95,6 @@ less, then the die is rolled again. Thus, '1d6r3' will only return a
 result of 4, 5, or 6. If the given value is larger than the number of
 sides on the die, then it defaults to the sides - 1.
 
-**\*#** - this denotes a multiplier to the result of the dice roll. Note
-that this option is applied **after** the dice have been rolled, coming 
-after any exploding or rerolling dice. Thus, '3d4 *10' will result in 
-a range of 30 to 120, while '3d4 r1 *10' will result in a range of 20 to
-120.
-
 Dice String Limitations
 -----------------------
 
@@ -84,6 +104,8 @@ die, which can be up to 3 digits.
 
 Using the Dice Library
 ----------------------
+
+**TODO:** Update this section!
 
 Using the library is rather straight forward:
 
@@ -115,26 +137,14 @@ For the above given dice string, would print something like this:
       5: 5
     1d6: 3
 
-Also, if you are curious to see how the complex dice string was parsed,
-you can retrieved the parsed value from the Roll instance using the
-parsed() method:
+Also, if you are curious to see how the dice string was parsed, you can 
+retrieved the parsed value from the Roll instance using the tree() method:
 
-    parsed = roll.parsed()
+    parsed = roll.tree()
 
 For the above given dice string, this returns a nested array of values:
 
-    [
-      [
-        [:label, "Damage"],
-        [:start, "2d8"],
-        [:add, 5],
-        [:add, "1d6"]
-      ]
-    ]
-
-...note, however, that each sections 2nd element is actually a class 
-instance of SimplePart or one of it's subclasses. I used strings above
-to simply show the format.
+    Put tree ouput here!
 
 Typically, you won't have to deal with the internals of a dice roll if all
 you want are the results. However, you can dig down into the returned
