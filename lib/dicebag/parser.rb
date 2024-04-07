fddf00bbbaee26e24f036644d7ee5a2512b4d662
Humblemonk <humblemonk@gmail.com>
2024-04-07 10:30:35 -0400

    Support up to d1000
    One line change to allow the parser to read d1000 rolls. This is
    required for some game systems.

--- lib/dicebag/parser.rb
+++ lib/dicebag/parser.rb
@@ -11,7 +11,7 @@ module DiceBag
     # Numbers are limited to 3 digit places. Why?
     # To prevent abuse from people rolling:
     # 999999999D999999999 and 'DOS'-ing the app.
-    rule(:number)  { match('[0-9]').repeat(1, 3) }
+    rule(:number)  { match('[0-9]').repeat(1, 4) }
     rule(:number?) { number.maybe }
 
     # Label rule
